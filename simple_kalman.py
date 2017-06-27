5eeaa56363971264fa2302d2feaa26dcdb108ca8
Jason Patterson <jpatterson7398@gmail.com>
2017-06-27 14:19:36 -0400

    fixed state transition model

--- simple_kalman.py
+++ simple_kalman.py
@@ -1,292 +1,12 @@
 import numpy as np
-from scipy.optimize import linear_sum_assignment
 import matplotlib.pyplot as plt
 import pandas
 import pdb
-from filterpy.kalman import KalmanFilter, UnscentedKalmanFilter, MerweScaledSigmaPoints, unscented_transform, JulierSigmaPoints
-from filterpy.common import Q_discrete_white_noise
+from filters import UnscentedKalmanTracker
 
-class DerivedUnscentedKalmanFilter(UnscentedKalmanFilter):
-    def __init__(self, dim_x, dim_z, dt, hx, fx, points):
-        UnscentedKalmanFilter.__init__(
-            self, dim_x=dim_x, dim_z=dim_z, dt=dt, hx=hx, fx=fx, points=points, 
-        )
 
-    def get_prediction(self, dt=None,  UT=None, fx_args=()):
-        if dt is None:
-            dt = self._dt
 
-        if not isinstance(fx_args, tuple):
-            fx_args = (fx_args,)
-
-        if UT is None:
-            UT = unscented_transform
-
-        # calculate sigma points for given mean and covariance
-        sigmas = self.points_fn.sigma_points(self.x, self.P)
-
-        for i in range(self._num_sigmas):
-            self.sigmas_f[i] = self.fx(sigmas[i], dt, *fx_args)
-
-        x, P = UT(self.sigmas_f, self.Wm, self.Wc, self.Q,
-                            self.x_mean, self.residual_x)
-        return x, P
-
-class UnscentedKalmanTracker(object):
-    """
-    Uses Kalman Filters and assignments with a Hungarian algorithm to keep track
-    of observed objects
-    """
-    def __init__(self, P0, Q, R, f, h, dt):
-        self.predictors = [] #List of KalmanThreads
-        self.current_predictor_label = 1
-        # {'label': 1, 'predictor': KalmanThread}
-        self.strikes = []
-
-        self.P0, self.Q, self.R, self.f, self.h, self.dt = P0, Q, R, f, h, dt
-
-        self.k = 0
-
-
-    def detect(self, observations):
-        # print [predictor['label'] for predictor in self.predictors]
-        #Remove a predictor if it has had too many erroneous walks
-        # for i, strikes in enumerate(self.strikes):
-        #   if strikes > 5:
-        #     del self.strikes[i]
-        #     del self.predictors[i]
-        #     self.current_predictor_label -= 1
-
-        #update each prediction and form a cost matrix
-
-
-        cost_matrix = np.zeros((len(observations), len(self.predictors)))
-
-        #rows of cost matrix represent observations, columns represent predictions
-        for i, observation_dict in enumerate(observations):
-            for j, predictor in enumerate(self.predictors):
-                z = observation_dict['z']
-                fx_args = observation_dict['fx_args']
-                # print 'here'
-                x0 = predictor['predictor'].x
-                x, P = predictor['predictor'].get_prediction(fx_args=fx_args)
-                # print "xtip:{}\tytip:{}\tpixlen:{}\tw:{}".format(x0[0], x0[1], x0[4], x0[5])
-                prediction = predictor['predictor'].hx(x)
-                # print prediction
-                # print 'there'
-                dist = np.linalg.norm(prediction - z)
-                detP = np.linalg.det(P)
-                cost = dist
-                # cost = dist * detP
-                # print cost
-                cost_matrix[i, j] = cost
-                # print detP_weight
-        # print 'here'
-        observation_indices, prediction_indices = linear_sum_assignment(cost_matrix)
-        # print 'there'
-        preds = []
-        for i in range(len(observation_indices)):
-            observation_index = observation_indices[i]
-            prediction_index = prediction_indices[i]
-
-            cost = cost_matrix[observation_index, prediction_index]
-            # print cost
-            # if cost < 30:
-            observation_dict = observations[observation_index]
-            z = observation_dict['z']
-            fx_args = observation_dict['fx_args']
-
-            self.predictors[prediction_index]['predictor'].predict(fx_args=fx_args)
-            
-            self.predictors[prediction_index]['predictor'].update(z)
-            # preds.append(self.predictors[prediction_index]['predictor'].hx(self.predictors[i]['predictor'].x))
-
-
-        # Prepare to add new observation if number exceeds predictions
-        if len(observations) > len(self.predictors):
-            mask = np.in1d(np.arange(len(observations)), observation_indices)
-
-            unused_indices = np.where(~mask)[0]
-            for i in unused_indices:  
-                observation_dict = observations[i]
-                x0 = observation_dict["x"]
-                self.addPredictor(x0)
-                prediction_indices = np.append(prediction_indices, i)
-
-        # if len(self.predictors) > len(observations):
-        #   mask = np.in1d(np.arange(len(self.predictors)), prediction_indices)
-
-        #   unused_indices = np.where(~mask)[0]
-
-        #   # for i in unused_indices:
-        #   #   self.predictors[i]['predictor'].P
-
-
-
-        #If cost of any assignment is too high, increment strikes...threshold is arbitrary 
-        for i, j in zip(observation_indices, prediction_indices):
-            cost = cost_matrix[i, j]
-            # print "i:{}\t j:{}\t cost:{}".format(i, j, cost)
-
-            if cost > 50:
-                self.strikes[j] += 1
-            else:
-                self.strikes[j] = 0
-
-        # Return the label (numerical) of each assignment
-        labels = [self.predictors[i]['label'] for i in prediction_indices]
-        preds = [self.predictors[i]['predictor'].hx(self.predictors[i]['predictor'].x) for i in prediction_indices]
-        return labels, preds
-
-    def addPredictor(self, x0):
-        dim_x = self.Q.shape[0]
-        dim_z = self.R.shape[0]
-        points_class = JulierSigmaPoints(dim_x, 0)
-
-        kf = DerivedUnscentedKalmanFilter(
-            dim_x=dim_x, dim_z=dim_z,
-            dt=self.dt, hx=self.h, fx=self.f,
-            points=points_class,
-        )
-        kf.x = x0
-        kf.P = self.P0
-        kf.R = self.R
-        kf.Q = self.Q
-
-        self.predictors.append(
-          {
-            'predictor' : kf,
-            'label' : self.current_predictor_label
-          }
-        )
-
-        self.strikes.append(0)
-
-        self.current_predictor_label += 1
-
-class KalmanTracker(object):
-    """
-    Uses Kalman Filters and assignments with a Hungarian algorithm to keep track
-    of observed objects
-    """
-    def __init__(self, P0, F, H, Q, R):
-        self.predictors = [] #List of KalmanThreads
-        self.current_predictor_label = 1
-        # {'label': 1, 'predictor': KalmanThread}
-        self.strikes = []
-
-        self.P0, self.Q, self.R, self.F, self.H = P0, Q, R, F, H
-
-        self.k = 0
-
-
-    def detect(self, observations):
-        # print [predictor['label'] for predictor in self.predictors]
-        #Remove a predictor if it has had too many erroneous walks
-        # for i, strikes in enumerate(self.strikes):
-        #   if strikes > 5:
-        #     del self.strikes[i]
-        #     del self.predictors[i]
-        #     self.current_predictor_label -= 1
-
-        #update each prediction and form a cost matrix
-
-
-        cost_matrix = np.zeros((len(observations), len(self.predictors)))
-
-        #rows of cost matrix represent observations, columns represent predictions
-        for i, observation_dict in enumerate(observations):
-            for j, predictor in enumerate(self.predictors):
-                z = observation_dict['z']
-
-                x, P = predictor['predictor'].get_prediction()
-
-                dist = np.linalg.norm(x - z)
-                detP = np.linalg.det(P)
-                cost = dist
-                # cost = dist * detP
-                # print cost
-                cost_matrix[i, j] = cost
-                # print detP_weight
-        # print 'here'
-        observation_indices, prediction_indices = linear_sum_assignment(cost_matrix)
-        # print 'there'
-
-        for i in range(len(observation_indices)):
-            observation_index = observation_indices[i]
-            prediction_index = prediction_indices[i]
-
-            cost = cost_matrix[observation_index, prediction_index]
-            # print cost
-            # if cost < 30:
-            observation_dict = observations[observation_index]
-            z = observation_dict['z']
-
-            self.predictors[prediction_index]['predictor'].predict()
-            self.predictors[prediction_index]['predictor'].update(z)
-
-
-        # Prepare to add new observation if number exceeds predictions
-        if len(observations) > len(self.predictors):
-            mask = np.in1d(np.arange(len(observations)), observation_indices)
-
-            unused_indices = np.where(~mask)[0]
-            for i in unused_indices:  
-                observation_dict = observations[i]
-                x0 = observation_dict["x"]
-                self.addPredictor(x0)
-                prediction_indices = np.append(prediction_indices, i)
-
-        # if len(self.predictors) > len(observations):
-        #   mask = np.in1d(np.arange(len(self.predictors)), prediction_indices)
-
-        #   unused_indices = np.where(~mask)[0]
-
-        #   # for i in unused_indices:
-        #   #   self.predictors[i]['predictor'].P
-
-
-
-        #If cost of any assignment is too high, increment strikes...threshold is arbitrary 
-        for i, j in zip(observation_indices, prediction_indices):
-            cost = cost_matrix[i, j]
-            # print "i:{}\t j:{}\t cost:{}".format(i, j, cost)
-
-            if cost > 50:
-                self.strikes[j] += 1
-            else:
-                self.strikes[j] = 0
-
-        # Return the label (numerical) of each assignment
-        labels = [self.predictors[i]['label'] for i in prediction_indices]
-        preds = [self.predictors[i]['predictor'].x for i in prediction_indices]
-
-        return labels, preds
-
-    def addPredictor(self, x0):
-        dim_x = self.Q.shape[0]
-        dim_z = self.R.shape[0]
-
-        kf = KalmanFilter(dim_x=dim_x, dim_z=dim_z)
-        kf.x = x0
-        kf.F = self.F
-        kf.H = self.H
-        kf.P = self.P0
-        kf.R = self.R
-        kf.Q = self.Q
-
-        self.predictors.append(
-          {
-            'predictor' : kf,
-            'label' : self.current_predictor_label
-          }
-        )
-
-        self.strikes.append(0)
-
-        self.current_predictor_label += 1
-
-def state_function(state, dt, omega, angle):
+def state_function(state, dt, angle, new_folx, new_foly):
     tipx, tipy, folx, foly, pixlen = state[0], state[1], state[2], state[3], state[4]
 
     theta = -1 * np.arctan((tipy - foly) / (tipx - folx))
@@ -294,14 +14,20 @@ def state_function(state, dt, omega, angle):
     # theta_new = theta + omega * dt
     # v = omega * pixlen
     omega = (angle - theta) / dt
+    # print omega * 180 / np.pi
     v = omega * pixlen
+
+    # print theta * 180 / np.pi, angle * 180 / np.pi
 
     # print np.cos(theta_new - theta)
     # tipx = tipx + np.cos(theta_new - theta) * pixlen
     # tipy = tipy + np.sin(theta_new - theta) * pixlen
     # print -v * np.cos(theta) * dt, -v * np.sin(theta) * dt
-    tipx = tipx - v * np.cos(theta + np.pi / 2) * dt
-    tipy = tipy + v * np.sin(theta) * dt
+
+    tipx = tipx + v * np.sin(theta) * dt + (new_folx - folx)
+    tipy = tipy + v * np.cos(theta) * dt + (new_foly - foly)
+
+
 
 
     # print midpoint * 180 / np.pi
@@ -333,11 +59,15 @@ dim_z = 5
 # R = np.eye(dim_z)
 # Q = np.eye(dim_x)
 
-P0 = np.eye(dim_x) * np.array([1, 1, 1, 0.5, 0.0001,])
-R = np.eye(dim_z) * np.array([1, 1, 0.0001, 0.0001, 5]) * 200
-Q = np.eye(dim_x) * np.array([1, 1, 0.0001, 0.0001, 0.0001,])
+# P0 = np.eye(dim_x) * np.array([1, 1, 1, 0.5, 0.0001,])
+# R = np.eye(dim_z) * np.array([1, 10, 0.0001, 0.0001, 1]) * 100000
+# Q = np.eye(dim_x) * np.array([1, 1, 0.0001, 0.0001, 0.0001,]) * 0.0001
 
-dt = 30 ** -1
+P0 = np.eye(dim_x) * np.array([1, 1, 1, 0.5, 0.0001,])
+R = np.eye(dim_z) * np.array([500, 500, 0.001, 0.001, 500,])
+Q = np.eye(dim_x) * np.array([1, 1, 0.1, 0.1, 0.1,]) * 500
+
+dt = 200 ** -1
 
 # tracker = KalmanTracker(
 #     P0=P0, Q=Q, R=R, F=F, H=H,
@@ -349,7 +79,7 @@ predictions = {}
 
 
 data = pandas.read_pickle('15000_frames_revised.pickle')
-data = data[(data.frame > 10000) & (data.frame < 10500) ]
+data = data[(data.frame > 10000) & (data.frame < 11000) ]
 
 data_filtered = data[data.pixlen > 20].groupby('frame')
 angles = data_filtered['angle'].apply(lambda x: x.mean()) * np.pi / 180
@@ -394,7 +124,7 @@ for frame, observations in data_filtered:
         observation_dicts.append({
             "x" : x0,
             "z" : z,
-            "fx_args" : (omega, angle)
+            "fx_args" : (angle, folx, foly)
         })
 
     labels, preds = tracker.detect(observation_dicts)
@@ -413,7 +143,7 @@ for frame, observations in data_filtered:
     #         end_frame = frame
 
 
-subset = data[ (data.frame > 10000) & (data.pixlen < 13000)].groupby('frame')
+subset = data[ (data.frame > 10000) & (data.frame < 15000)].groupby('frame')
 
 plt.ion()
 for frame, whiskers in subset:
@@ -430,15 +160,16 @@ for frame, whiskers in subset:
     for key in preds.keys():
         z = preds[key]
         color = whisker_colors[int(key)]
-        tipx, tipy, foly  = z[0], z[1], z[2]
+        tipx, tipy, folx, foly  = z[0], z[1], z[2], z[3]
         plt.plot(tipx, tipy, marker='o', color=color, markersize=15)
+        plt.plot(folx, foly, marker='o', color=color, markersize=15)
 
 
     for i, whisker in whiskers.iterrows():
         folx, foly = whisker['fol_x'], whisker['fol_y']
         tipx, tipy = whisker['tip_x'], whisker['tip_y']
-        # angle = whisker['angle'] * np.pi / 180
-        # print"Calculated angle:{}\tMeasured angle:{}".format(np.arctan((tipy - foly) / (tipx - folx)), angle)
+        # angle = whisker['angle']
+        # print"Calculated angle:{}\tMeasured angle:{}".format(-1 * np.arctan((tipy - foly) / (tipx - folx)) * 180 / np.pi, angle)
 
         color = whisker_colors[int(whisker['color_group'])]
 
